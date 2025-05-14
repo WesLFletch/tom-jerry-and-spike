e0f25c20e62c3d1c336eb5a91e2806ae3ff2b4ca
WesLFletch <weslfletch@tamu.edu>
2025-05-14 14:25:53 -0500

    WF JerryBotRational - Re-Fixed importing from parent dir
    My last attempt at importing from the parent directory was unsuccessful. This has fixed it on my local version, but I'd like someone else to try it on theirs too, just to be sure.

--- JerryVersions/JerryBotRational.py
+++ JerryVersions/JerryBotRational.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from texasholdem import ActionType, Card, PlayerState
 from texasholdem.evaluator import evaluate
 from PokerBot import PokerBot
